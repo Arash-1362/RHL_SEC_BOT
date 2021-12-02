6d0a5ec7128cc76a6cab1f512ad6903c1cbaf710
Arash-1362 <arashdoust@gmail.com>
2021-12-02 12:13:54 +0000

    Add ipSecurityDialog

--- Desktop/RHL_SEC1/componentDialogs/ipSecurityDialog.js
+++ Desktop/RHL_SEC1/componentDialogs/ipSecurityDialog.js
@@ -1,3 +1,5 @@
+//template is used from https://github.com/microsoft/BotBuilder-Samples
+//to be implemented .....
 const {WaterfallDialog, ComponentDialog } = require('botbuilder-dialogs');
 
 const {ConfirmPrompt, ChoicePrompt, DateTimePrompt, NumberPrompt, TextPrompt  } = require('botbuilder-dialogs');
@@ -34,8 +36,9 @@ class IPSecurityDialog extends ComponentDialog { // Reuseable  dialoge component
 this.addDialog(new WaterfallDialog(WATERFALL_DIALOG, [  
 
     this.firstStep.bind(this),  // Ask confirmation if user wants to use the service?
-    this.confirmStep.bind(this), // Show summary of values entered by user and ask confirmation to make reservation
-    this.summaryStep.bind(this),  //Show summary of values entered by user and ask confirmation to make reservation
+    this.getName.bind(this),    // Get name of URL
+    this.confirmStep.bind(this), // Show summary of values entered by user and ask confirmation 
+    this.summaryStep.bind(this),  //Show summary of values entered by user and ask confirmation 
 ]));
 
     this.initialDialogId = WATERFALL_DIALOG;
@@ -63,6 +66,24 @@ return await step.prompt(CONFIRM_PROMPT, 'Would you like to use a service ', ['y
 
 
 
+async getName(step){ //Prompt user to enter the IP to be scaned 
+     
+    console.log(step.result)
+
+    if(step.result === true)
+    { 
+    return await step.prompt(TEXT_PROMPT, 'What is the IP you need to scan ?');
+    }
+    if(step.result === false)
+    { 
+        await step.context.sendActivity("You chose not to go ahead with Service.");
+        endDialog = true;
+        return await step.endDialog();   
+    }
+
+
+}
+
 
 async confirmStep(step){
 
@@ -72,18 +93,19 @@ async confirmStep(step){
 
     await step.context.sendActivity(msg);
 
-    return await step.prompt(CONFIRM_PROMPT, 'Are you sure IP is correct and you want to go ahead?', ['yes', 'no']);
+    return await step.prompt(CONFIRM_PROMPT, 'Are you sure IP  is correct and you want to go ahead?', ['yes', 'no']);
 }
 
 
 
+//Summary of the steps will return the summary and the resualt of the requiest to user 
 
  summaryStep(step){
 
 
-    if(step.result===true)
+    if(step.result===true) 
     {
-        const nvt = require('node-virustotal');
+        const nvt = require('node-virustotal');// Resualt of safe or unsafe IP submitted by user will be shown by calling Adabtive cards(to be implemented)
         const defaultTimedInstance = nvt.makeAPI().setKey('5d0b82b762587006ac0c6bb4197101c8df992dfd08fac4ecaf31b047aa76e866');
         const theSameObject = defaultTimedInstance.ipLookup(  IPtoBeScaned, function(err, res){
        var road = JSON.parse(res);
